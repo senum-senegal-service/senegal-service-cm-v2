bc32350a3ec676996a1e4ad9b35d22a05602360e
genieouz <33516121+genieouz@users.noreply.github.com>
2024-04-17 18:29:42 +0000

    use senum realm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,16 @@
+// export const environment = {
+//   GRAPHQL_URI: "http://localhost:5001/graphql",
+//   API_URI: "http://localhost:5001",
+//   KEYCLOAK_URL: "https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443",
+//   KEYCLOACK_REALM: "teste",
+//   KEYCLOACK_CLIENT_ID: "teste"
+// };
+
 export const environment = {
   GRAPHQL_URI: "http://localhost:5001/graphql",
   API_URI: "http://localhost:5001",
-  KEYCLOAK_URL: "https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443",
-  KEYCLOACK_REALM: "teste",
-  KEYCLOACK_CLIENT_ID: "teste"
+  KEYCLOAK_URL: "https://devauth.sec.gouv.sn",
+  KEYCLOACK_REALM: "senegalservices",
+  KEYCLOACK_CLIENT_ID: "senegalservice"
 };
 // http://localhost:5001
