4f2c844fd857a29461b47f298f781bd657f83dc2
genieouz <33516121+genieouz@users.noreply.github.com>
2024-04-17 18:30:15 +0000

    use senum realm

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   GRAPHQL_URI: "https://senegal-service-cm-api.onrender.com/graphql",
   API_URI: "https://senegal-service-cm-api.onrender.com",
-  KEYCLOAK_URL: "https://ec2-16-171-29-127.eu-north-1.compute.amazonaws.com:8443",
-  KEYCLOACK_REALM: "teste",
-  KEYCLOACK_CLIENT_ID: "teste"
+  KEYCLOAK_URL: "https://devauth.sec.gouv.sn",
+  KEYCLOACK_REALM: "senegalservices",
+  KEYCLOACK_CLIENT_ID: "senegalservice"
 };
